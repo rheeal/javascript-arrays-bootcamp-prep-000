654d0123fd019bb934358f11c83036896a60d1ba
Allen Rhee <rhee.daehyun@gmail.com>
2018-08-22 03:30:41 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,40 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+}
+
+function accessElementInArray (array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+}
+
+
+
+
+
